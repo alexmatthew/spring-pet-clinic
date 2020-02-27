52d2cb225e7e2d323f99784d3c79a7224947ec58
Alex <candelarioac@gmail.com>
2020-02-27 15:03:38 -0500

    closes another ticket

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
-    public Owner(String firstName, String lastName) {
+public class Owner extends Person{
+    private String firstName;
+    private String lastName;
+
+    public Owner(String firstName, String lastName, String firstName1, String lastName1) {
         super(firstName, lastName);
+        this.firstName = firstName1;
+        this.lastName = lastName1;
     }
 }
