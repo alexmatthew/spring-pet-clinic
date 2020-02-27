288bf1583f7314d85a03a34850bf8472809ce7db
Alex <candelarioac@gmail.com>
2020-02-27 14:58:48 -0500

    Added Services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Owner extends Person{
+public class Vet extends Person{
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
     }
